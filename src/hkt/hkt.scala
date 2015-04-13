dc2ce971db84a5fb933082d3d7b10dad3c276a8d
Alexander Samoilov <alexander.samoilov@gmail.com>
2015-04-13 15:19:37 +0300

    test suite with example usage for Functor.

--- src/hkt/hkt.scala
+++ src/hkt/hkt.scala
@@ -1,73 +1,26 @@
 package hkt 
 
-//
-// http://stackoverflow.com/questions/19880207/examples-of-applicative-functor-usage-in-scala
-// see also scalaz
-// see also algebird of Twitter
+import scala.language.higherKinds
 
 trait Functor[F[_]] {
   def fmap[T, U](m: F[T])(fn: (T) => U): F[U]
 }
 
+object Functor {
+  def apply[F[_]](implicit f: Functor[F]) = f
+}
+
 trait Applicative[F[_]] extends Functor[F] {
 
-  def apply[A, B](f: F[A => B]): F[A] => F[B]
+  def apply[A, B](f: F[A => B]): F[A] => F[B] // its synonim ia a <*>
 
-  def point[A](a: A): F[A]
+  def <*>[A, B](f: F[A => B]): F[A] => F[B] = apply(f)
 
-  def pure[A](a: A): F[A] = point(a)
+  def point[A](a: => A): F[A]
+
+  def pure[A](a: => A): F[A] = point(a)
 }
 
 object Applicative {
-
+  def apply[F[_]](implicit a: Applicative[F]) = a
 }
-
-//
-// http://stackoverflow.com/questions/19880207/examples-of-applicative-functor-usage-in-scala
-// see also scalaz
-// see also algebird of Twitter
-//trait GenericFunctor[->>[_, _], ->>>[_, _], F[_]] {
-//
-//  def fmap[A, B](f: A ->> B): F[A] ->>> F[B]
-//}
-//
-//trait Functor[F[_]] extends GenericFunctor[Function, Function, F] {
-//
-//  final def fmap[A, B](as: F[A])(f: A => B): F[B] =
-//    fmap(f)(as)
-//}
-
-//trait Applicative[F[_]] extends Functor[f] {
-//
-//  def point[A](a: A): F[A]
-//
-//  def pure[A](a: A): F[A] = point(a)
-//
-//  def apply[A, B](f: F[A => B]): F[A] => F[B]
-//
-//  final def apply[A, B](fa: F[A])(f: F[A => B]): F[B] =
-//    apply(f)(fa)
-//
-//  override def fmap[A, B](f: A => B): F[A] => F[B] =
-//    apply(pure(f))
-//}
-//
-//object Applicative {
-//
-//  def pure[A, F[_]](a: A)(implicit applicative: Applicative[F]): F[A] =
-//    applicative pure a
-//
-//  def apply[A, B, F[_]](fa: F[A])(f: F[A => B])(implicit applicative: Applicative[F]): F[B] =
-//    applicative.apply(fa)(f)
-//
-//  implicit object OptionApplicative extends Applicative[Option] {
-//
-//    override def pure[A](a: A): Option[A] =
-//      Option(a)
-//
-//    override def apply[A, B](f: Option[A => B]): Option[A] => Option[B] =
-//      o => for { a <- o; p <- f } yield p(a)
-//    }
-//}
-
-
--- test/hkt_test.scala
+++ test/hkt_test.scala
@@ -1,14 +1,23 @@
 import org.scalatest.FunSuite
 
-class SetSuite extends FunSuite {
+class MySuite extends FunSuite {
+
+  import hkt._
+
+  implicit val listFunctor: Functor[List] = new Functor[List] {
+    def fmap[A, B](fa: List[A])(f: A => B): List[B] = fa.map(f)
+  }
+   
 
   trait Sample {
+    val id = 0
   }
 
   test("An empty Set should have size 0") {
-    new Sample {
+    val sample = new Sample {
       assert(Set.empty.size == 0)
     }
+    assert(sample.id == 0)
   }
 
   test("Invoking head on an empty Set should produce NoSuchElementException") {
@@ -17,8 +26,26 @@ class SetSuite extends FunSuite {
     }
   }
 
-  test("A real test for plying with hkt") {
+  test("Functor inc test") {
+    def inc(list: List[Int])(implicit func: Functor[List]) = func.fmap(list)(_ + 1)
+    assert(inc(List(1, 2, 3)) == List(2, 3, 4))
+  }
 
+  test("Functor identity law test") {
+    val expectedList = List(1, 2, 3)
+    val outputList = Functor[List].fmap(expectedList)(x => x)
+    assert(expectedList == outputList)
+  }
+
+  test("Functor composition test") {
+    val f1 = (x: Int) => x + 1
+    val f2 = (x: Int) => x * 2
+    val inputList = List(1, 2, 3)
+
+    val compRes = Functor[List].fmap(inputList)(f2 compose f1)
+    val mapRes = Functor[List].fmap(Functor[List].fmap(inputList)(f1))(f2)
+    
+    assert(compRes == mapRes)
   }
 
 }
